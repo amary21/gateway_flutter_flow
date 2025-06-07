5fcf296a593522592cb0dfa1458bb4629af7a9a9
T-Amary <taufik.amary@gmail.com>
2025-06-07 17:32:18 +0700

    Integrate Adjust SDK for iOS and update project configuration
    - Added deeplink handling methods using Adjust SDK in `AppDelegate.swift`.
    - Upgraded Gradle wrapper to 8.3 and Android Gradle plugin to 8.1.0.
    - Updated Kotlin version to 1.8.22 and set JVM target to 1.8.
    - Adjusted `.gitignore` to exclude `.cxx` files.

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,4 +1,5 @@
 import Flutter
+import AdjustSdk
 import UIKit
 
 @main
@@ -9,5 +10,25 @@ import UIKit
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+
+  override func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+    NSLog("Scheme based deep link opened an app: %@", url.absoluteString)
+    // add your code below to handle deep link
+    // (e.g., open deep link content)
+    // url object contains the deep link
+
+    // Call the below method to send deep link to Adjust backend
+    Adjust.processDeeplink(ADJDeeplink(deeplink: url)!)
+    return true
+  }
+
+  override func application(_ application: UIApplication, continue userActivity: NSUserActivity, restorationHandler: @escaping ([UIUserActivityRestoring]?) -> Void) -> Bool {
+    if (userActivity.activityType == NSUserActivityTypeBrowsingWeb) {
+      NSLog("Universal link opened an app: %@", userActivity.webpageURL!.absoluteString)
+      // Pass deep link to Adjust in order to potentially reattribute user.
+      Adjust.processDeeplink(ADJDeeplink(deeplink: userActivity.webpageURL!)!)
+    }
+    return true
   }
 }
