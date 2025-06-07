ad320ebdd0979dc790ec2bcbb73297e9f47e123e
T-Amary <taufik.amary@gmail.com>
2025-06-07 16:47:08 +0700

    **Add deeplink handling for Android with Adjust SDK**
    - Implemented deeplink processing in `MainActivity` using Adjust SDK.
    - Added intent filters to `AndroidManifest.xml` for handling view actions via custom scheme.

--- android/app/src/main/kotlin/com/example/existing_flutter_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/existing_flutter_app/MainActivity.kt
@@ -1,5 +1,22 @@
 package com.example.existing_flutter_app
 
 import io.flutter.embedding.android.FlutterActivity
+import android.content.Intent
+import android.os.Bundle
+import com.adjust.sdk.Adjust
+import com.adjust.sdk.AdjustDeeplink
 
-class MainActivity: FlutterActivity()
+class MainActivity: FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val intent: Intent = intent
+        val data = intent.data
+        Adjust.processDeeplink(AdjustDeeplink(data), this)
+    }
+
+    override fun onNewIntent(intent: Intent) {
+        super.onNewIntent(intent)
+        val data = intent?.data
+        Adjust.processDeeplink(AdjustDeeplink(data), this)
+    }
+}
